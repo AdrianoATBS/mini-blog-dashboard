14355e6955bab0b3a23ba168662fbda8ec703c6e
Adriano Teixeira Benevenuto Sampaio <adrianoteixeira1223@gmail.com>
2026-01-23 18:19:53 -0300

    feat(botao): Cria botao generico e estrutura o layout do cabecalho

--- src/components/Botao/Botao.tsx
+++ src/components/Botao/Botao.tsx
@@ -1,8 +1,15 @@
+type BotaoProps = {
+    texto: string;
+    onClick?: () => void;
+};
 
-
-export default function Botao() {
+export default function Botao({ texto, onClick }: BotaoProps) {
     return(
         <>
+            <button onClick={onClick} className="border bg-acoes-primaria rounded-full 
+            px-4 text-white  hover:bg-hover hover:transform hover:scale-95">{texto}
+            </button>
+           
         </>
     )
 }
--- src/components/Cabecalho/Cabecalho.tsx
+++ src/components/Cabecalho/Cabecalho.tsx
@@ -1,7 +1,7 @@
 'use client';
 import { CiSearch } from "react-icons/ci";
 import { useState } from "react";
-
+import Botao from "../Botao/Botao";
 export default function Cabecalho() {
     const [pesquisa, setPesquisa] = useState('');
 
@@ -16,20 +16,30 @@ export default function Cabecalho() {
     return (
         <>
             <header className="px-6 py-4 bg-fundo-secundario">
-                <div className="flex items-center justify-center relative mx-auto w-full max-w-56 ">
-                    <input
-                        type="text"
-                        value={pesquisa}
-                        onChange={handlePesquisaChange}
-                        placeholder="Pesquisa..."
-                        className="w-full p-2 pl-5 pr-12 border border-borda rounded-full focus:outline-none focus:ring-1 focus:ring-acoes-primaria text-xs"
-                    />
-                
-                    <CiSearch
-                        className="absolute right-5 top-1/2 transform -translate-y-1/2 text-acoes-primaria cursor-pointer"
-                        onClick={handlePesquisaSubmit}
-                    />
+                <div className="flex items-center ">
+                    <div className="flex-1"/>
+                    <div className="relative w-full max-w-56">
+
+                        <input
+                            type="text"
+                            value={pesquisa}
+                            onChange={handlePesquisaChange}
+                            placeholder="Pesquisa..."
+                            className="w-full p-2 pl-5 pr-12 border border-borda rounded-full focus:outline-none focus:ring-1 focus:ring-acoes-primaria text-xs"
+                        />
+                    
+                        <CiSearch
+                            className="absolute right-5 top-1/2 transform -translate-y-1/2 text-acoes-primaria cursor-pointer"
+                            onClick={handlePesquisaSubmit}
+                        />
+                    </div>
+                    <div className="flex flex-1 justify-end gap-3">
+                        <Botao texto="Registrar"/>
+                        <Botao texto="Login"/>
+                    </div>
+                   
                 </div>
+
             </header>
         </>
     );
