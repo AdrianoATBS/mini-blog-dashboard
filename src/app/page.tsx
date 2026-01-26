9b6e665f23e68712c1cea574673115d220483d75
Adriano Teixeira Benevenuto Sampaio <adrianoteixeira1223@gmail.com>
2026-01-26 18:10:24 -0300

    feat(PostCard): cria componente PostCard e renderiza lista de posts na Home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,26 @@
 import Cabecalho from "@/components/Cabecalho/Cabecalho";
 import Image from "next/image";
 import PostCard from "@/components/PostCard/PostCard";
+import { postsMock } from "../mocks/posts.mock";
+import { usersMock } from "../mocks/users.mock";
 export default function Home() {
   return (
-     <div>
+     <div >
         <Cabecalho />
-        <PostCard/>
+      <main className="p-6 grid gap-4 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+        {postsMock.map((post) =>
+         {
+            const user = usersMock.find((user) => user.id === post.userId);
+            return(
+               <PostCard
+                  key={post.id}
+                  titulo={post.title}
+                  conteudo={post.body}
+                  autor={user ? user.name : "Autor Desconhecido"}
+               />
+            )
+         })}
+      </main>
      </div>
   );
 }
--- src/components/PostCard/PostCard.tsx
+++ src/components/PostCard/PostCard.tsx
@@ -1,11 +1,15 @@
-
-
-export default function PostCard() {
+interface PostCardProps {
+    titulo: string;
+    conteudo: string;
+    autor: string;
+}
+export default function PostCard({titulo, conteudo, autor}: PostCardProps) {
     return(
         <>
-            <section>
-                    <h2>Dados</h2>
-                    <p>Conteúdo do post</p>
+            <section className=" bg-card p-4 rounded-lg border  border-borda">
+                    <h2 className="text-titulo font-semibold">{titulo}</h2>
+                    <p className="text-texto-secundario text-sm mt-1">{conteudo}</p>
+                    <span className="text-xs text-acoes-secundaria mt-2 block">{autor}</span>
             </section>
         </>
     )
