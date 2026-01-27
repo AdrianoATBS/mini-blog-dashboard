801ce3006a3fee213d5bb5e4cc5209cd95e67342
Adriano Teixeira Benevenuto Sampaio <adrianoteixeira1223@gmail.com>
2026-01-27 17:46:19 -0300

    feat(page): implementa paginação na página home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,28 @@
+'use client'
 import Cabecalho from "@/components/Cabecalho/Cabecalho";
-import Image from "next/image";
 import PostCard from "@/components/PostCard/PostCard";
 import { postsMock } from "../mocks/posts.mock";
 import { usersMock } from "../mocks/users.mock";
+import { useState } from "react";
 export default function Home() {
+
+   const [paginaAtual, setPaginaAtual] = useState(1);
+   const postsPorPagina = 9;
+
+   const indiceUltimoPost = paginaAtual * postsPorPagina;
+   const indicePrimeiroPost = indiceUltimoPost - postsPorPagina;
+   const postsAtuais = postsMock.slice(indicePrimeiroPost, indiceUltimoPost);
+
+   const totalPaginas = Math.ceil(postsMock.length / postsPorPagina);
+
+
   return (
      <div >
         <Cabecalho />
-      <main className="p-6 grid gap-4 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-        {postsMock.map((post) =>
+
+      <main className="p-6 ">
+        <div className="p-6 grid gap-4 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+        {postsAtuais.map((post) =>
          {
             const user = usersMock.find((user) => user.id === post.userId);
             return(
@@ -20,6 +34,21 @@ export default function Home() {
                />
             )
          })}
+         </div>
+         <div className=" flex justify-center items-center gap-6 mt-8">
+         <button onClick={() => setPaginaAtual(paginaAtual - 1)}
+            className="bg-acoes-primaria rounded-full p-2 text-white 
+            cursor-pointer hover:transform hover:scale-95"
+            disabled={paginaAtual === 1}
+            >Anterior</button>
+         <span className="text-texto-principal text-center">Página {paginaAtual} de {totalPaginas}</span>
+
+         <button onClick={() => setPaginaAtual(paginaAtual + 1)}
+            className="bg-acoes-primaria rounded-full p-2 text-white 
+            cursor-pointer hover:transform hover:scale-95"
+            disabled={paginaAtual === totalPaginas}
+            >Próxima</button>
+         </div>
       </main>
      </div>
   );
--- src/components/PostCard/PostCard.tsx
+++ src/components/PostCard/PostCard.tsx
@@ -1,4 +1,4 @@
-interface PostCardProps {
+    interface PostCardProps {
     titulo: string;
     conteudo: string;
     autor: string;
@@ -6,11 +6,11 @@ interface PostCardProps {
 export default function PostCard({titulo, conteudo, autor}: PostCardProps) {
     return(
         <>
-            <section className=" bg-card p-4 rounded-lg border  border-borda">
+            <section className=" bg-card p-4 rounded-lg border border-borda">
                     <h2 className="text-titulo font-semibold">{titulo}</h2>
                     <p className="text-texto-secundario text-sm mt-1">{conteudo}</p>
                     <span className="text-xs text-acoes-secundaria mt-2 block">{autor}</span>
-            </section>
+                </section>
         </>
     )
 }
