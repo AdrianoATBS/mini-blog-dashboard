dd7ee91e047bcf594f5a48917491f6fca515ff69
Adriano Teixeira Benevenuto Sampaio <adrianoteixeira1223@gmail.com>
2026-02-03 18:17:46 -0300

    docs: move interface UserPefil para pasta de types global

---
+++ src/types/UserPerfil.ts
@@ -0,0 +1,10 @@
+export interface UserPerfil {
+    name: string;
+    email: string;
+    phone: string;
+    company: {
+        name: string;
+        catchPhrase: string;
+    }
+    website: string;
+}
